21fca7efa9ff6af3c977dcd1197fa5dee22401fb
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-05-26 15:52:56 +0200

    Updates

---
+++ precache-manifest.bd7e5384e5f89c3646509cecc7b4dd69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c0a585b511e69959f0384a77638343",
+    "url": "/react-components/index.html"
+  },
+  {
+    "revision": "f10944e0d18bfb9e173e",
+    "url": "/react-components/static/css/2.deb05650.chunk.css"
+  },
+  {
+    "revision": "fe5085184af453fe4990",
+    "url": "/react-components/static/css/main.f41ca855.chunk.css"
+  },
+  {
+    "revision": "f10944e0d18bfb9e173e",
+    "url": "/react-components/static/js/2.2ad849bf.chunk.js"
+  },
+  {
+    "revision": "fe5085184af453fe4990",
+    "url": "/react-components/static/js/main.3316fb49.chunk.js"
+  },
+  {
+    "revision": "38d33e341a5d4ca40c70",
+    "url": "/react-components/static/js/runtime~main.19159b66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-components/static/media/logo.5d5d9eef.svg"
+  }
+]);
