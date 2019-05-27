63c1d6d46ffea29b29fb0a59cdf36f668e5bddc0
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-05-27 09:17:43 +0200

    Added links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./Home";
 import StepForm from "./stepform_component/StepForm";
 import stepformContent from "./stepform_component/stepform2.json";
+import Nav from "./navigation";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const fields = stepformContent.attachment.payload.fields;
@@ -9,6 +10,8 @@ const fields = stepformContent.attachment.payload.fields;
 export default props => {
   return (
     <Router>
+      <Nav />
+
       <Route path="/" exact component={Home} />
       <Route
         path="/stepform"
